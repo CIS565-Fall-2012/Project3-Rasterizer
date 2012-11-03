cf149db0005f4641157bf0c5bb22bf1ad0aea40d
Liam Boone <liamboone@gmail.com>
2012-11-03 03:59:36 -0400

    Psychedelic Cow with backface culling, still Zfighting, perspective Tform is odd

--- src/rasterizeKernels.cu
+++ src/rasterizeKernels.cu
@@ -130,9 +130,21 @@ __global__ void sendImageToPBO(uchar4* PBOpos, glm::vec2 resolution, glm::vec3* 
 }
 
 //TODO: Implement a vertex shader
-__global__ void vertexShadeKernel(float* vbo, int vbosize){
+__global__ void vertexShadeKernel(float* vbo, int vbosize, glm::mat4 mM, glm::mat4 vM, glm::mat4 pM){
   int index = (blockIdx.x * blockDim.x) + threadIdx.x;
-  if(index<vbosize/3){
+  if(index<vbosize/3)
+  {
+	  int idxX = index*3;
+	  int idxY = idxX + 1;
+	  int idxZ = idxX + 2;
+
+	  glm::vec4 v( vbo[idxX], vbo[idxY], vbo[idxZ], 1.0f );
+	  //v = v*mM*vM*pM;
+	  v = pM*vM*mM*v;
+	  
+	  vbo[idxX] = v.x/v.w;
+	  vbo[idxY] = v.y/v.w;
+	  vbo[idxZ] = v.z/v.w;
   }
 }
 
@@ -141,6 +153,30 @@ __global__ void primitiveAssemblyKernel(float* vbo, int vbosize, float* cbo, int
   int index = (blockIdx.x * blockDim.x) + threadIdx.x;
   int primitivesCount = ibosize/3;
   if(index<primitivesCount){
+	  int idx0 = index*3;
+	  int idx1 = idx0 + 1;
+	  int idx2 = idx0 + 2;
+
+	  int idxX = ibo[idx0]*3;
+	  int idxY = idxX + 1;
+	  int idxZ = idxY + 2;
+	  
+	  primitives[ index ].p0 = glm::vec3( vbo[idxX], vbo[idxY], vbo[idxZ] );
+	  primitives[ index ].c0 = glm::vec3( cbo[idxX%cbosize], cbo[idxY%cbosize], cbo[idxZ%cbosize] );
+
+	  idxX = ibo[idx1]*3;
+	  idxY = idxX + 1;
+	  idxZ = idxY + 2;
+
+	  primitives[ index ].p1 = glm::vec3( vbo[idxX], vbo[idxY], vbo[idxZ] );
+	  primitives[ index ].c1 = glm::vec3( cbo[idxX%cbosize], cbo[idxY%cbosize], cbo[idxZ%cbosize] );
+
+	  idxX = ibo[idx2]*3;
+	  idxY = idxX + 1;
+	  idxZ = idxY + 2;
+
+	  primitives[ index ].p2 = glm::vec3( vbo[idxX], vbo[idxY], vbo[idxZ] );
+	  primitives[ index ].c2 = glm::vec3( cbo[idxX%cbosize], cbo[idxY%cbosize], cbo[idxZ%cbosize] );
   }
 }
 
@@ -148,6 +184,60 @@ __global__ void primitiveAssemblyKernel(float* vbo, int vbosize, float* cbo, int
 __global__ void rasterizationKernel(triangle* primitives, int primitivesCount, fragment* depthbuffer, glm::vec2 resolution){
   int index = (blockIdx.x * blockDim.x) + threadIdx.x;
   if(index<primitivesCount){
+	  glm::vec2 p0,p1,p2;
+	  
+
+	  p0 = primitives[index].p0.swizzle(glm::X,glm::Y);
+	  p1 = primitives[index].p1.swizzle(glm::X,glm::Y);
+	  p2 = primitives[index].p2.swizzle(glm::X,glm::Y);
+	  
+	  
+//	  depthbuffer[(int)(floor(minP.x)+resolution.x*floor(minP.y))].color = primitives[index].c0;
+//	  depthbuffer[(int)(floor(maxP.x)+resolution.x*floor(maxP.y))].color = primitives[index].c2;
+
+	  glm::vec2 rA = p1-p0;
+	  glm::vec2 rB = p2-p0;
+
+	  if( glm::cross( glm::vec3( rA, 0.0f ), glm::vec3( rB, 0.0f ) ).z <= 0 )
+		  return;
+
+
+	  glm::vec2 minP = glm::max( glm::vec2( 0 ), glm::min( p0, glm::min( p1, p2 ) ) );
+	  glm::vec2 maxP = glm::min( resolution - glm::vec2( 1 ), glm::max( p0, glm::max( p1, p2 ) ) );
+
+	  glm::vec3 tmp = glm::cross( glm::vec3( 0, 0, 1 ), glm::vec3( rA, 0.0f ) );
+	  glm::vec2 vA(tmp.x, tmp.y);
+
+	  tmp = glm::cross( glm::vec3( 0, 0, 1 ), glm::vec3( rB, 0.0f ) );
+	  glm::vec2 vB(tmp.x, tmp.y);
+
+
+
+	  float lengthA = glm::length( rA );
+	  float lengthB = glm::length( rB );
+
+	  for( int y = minP.y; y <= maxP.y; y++ )
+	  {
+		  for( int x = minP.x; x <= maxP.x; x++ )
+		  {
+			  glm::vec2 p = glm::vec2( x, y ) - p0;
+			  float lambdaA = glm::dot( p, vA ) / glm::dot( rB, vA );
+			  float lambdaB = glm::dot( p, vB ) / glm::dot( rA, vB );
+			  if( lambdaA >= 0 && lambdaB >= 0 && lambdaA + lambdaB <= 1 )
+			  {
+				  float depth = primitives[index].p0.z*lambdaA + 
+								primitives[index].p1.z*lambdaB + 
+								primitives[index].p2.z*(1-lambdaA-lambdaB);
+				  if( depth > depthbuffer[(int)(x+resolution.x*y)].position.z )
+				  {
+					  depthbuffer[(int)(x+resolution.x*y)].position.z = depth;
+					  depthbuffer[(int)(x+resolution.x*y)].color = primitives[index].c0*lambdaA + 
+																   primitives[index].c1*lambdaB + 
+																   primitives[index].c2*(1-lambdaA-lambdaB);
+				  }
+			  }
+		  }
+	  }
   }
 }
 
@@ -157,6 +247,7 @@ __global__ void fragmentShadeKernel(fragment* depthbuffer, glm::vec2 resolution)
   int y = (blockIdx.y * blockDim.y) + threadIdx.y;
   int index = x + (y * resolution.x);
   if(x<=resolution.x && y<=resolution.y){
+
   }
 }
 
@@ -174,6 +265,14 @@ __global__ void render(glm::vec2 resolution, fragment* depthbuffer, glm::vec3* f
 
 // Wrapper for the __global__ call that sets up the kernel calls and does a ton of memory management
 void cudaRasterizeCore(uchar4* PBOpos, glm::vec2 resolution, float frame, float* vbo, int vbosize, float* cbo, int cbosize, int* ibo, int ibosize){
+
+
+	glm::mat4 modelMatrix = glm::rotate( glm::mat4(1.0f), frame/3, glm::vec3( 0, 1, 0 ) );
+	glm::mat4 projectionMatrix = glm::scale(
+		glm::translate(
+		glm::perspective( 45.0f, resolution.x/resolution.y, 0.1f, 100.0f ),
+		glm::vec3(resolution, 0.0f)),glm::vec3(-resolution,1.0f));
+	glm::mat4 viewMatrix = glm::lookAt( glm::vec3( 0, 0, 1 + frame/100 ), glm::vec3( 0, 0, 0 ), glm::vec3( 0, 1, 0 ) );
 
   // set up crucial magic
   int tileSize = 8;
@@ -221,7 +320,7 @@ void cudaRasterizeCore(uchar4* PBOpos, glm::vec2 resolution, float frame, float*
   //------------------------------
   //vertex shader
   //------------------------------
-  vertexShadeKernel<<<primitiveBlocks, tileSize>>>(device_vbo, vbosize);
+  vertexShadeKernel<<<primitiveBlocks, tileSize>>>(device_vbo, vbosize, modelMatrix, viewMatrix, projectionMatrix);
 
   cudaDeviceSynchronize();
   //------------------------------
@@ -237,10 +336,12 @@ void cudaRasterizeCore(uchar4* PBOpos, glm::vec2 resolution, float frame, float*
   rasterizationKernel<<<primitiveBlocks, tileSize>>>(primitives, ibosize/3, depthbuffer, resolution);
 
   cudaDeviceSynchronize();
+  checkCUDAError("Rasterizer");
   //------------------------------
   //fragment shader
   //------------------------------
   fragmentShadeKernel<<<fullBlocksPerGrid, threadsPerBlock>>>(depthbuffer, resolution);
+  checkCUDAError("Frag Shader");
 
   cudaDeviceSynchronize();
   //------------------------------
